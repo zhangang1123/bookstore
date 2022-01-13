51cd7c1d04a913600b0274a564f7b9e36e3ea978
angs <1684505291@qq.com>
2022-01-13 14:53:21 +0800

    ll

--- doc/前端/bookstore/src/main.js
+++ doc/前端/bookstore/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vueRouter from 'vue-router'
 import router from './router'
-//import store from './store'
+// import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
@@ -10,7 +10,7 @@ Vue.use(ElementUI);
 Vue.use(vueRouter);
 new Vue({
   router,
-  //store,
+ // store,
   render: h => h(App),
    beforeCreate(){
       Vue.prototype.$bus= this
